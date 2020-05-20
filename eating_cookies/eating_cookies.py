ec5fac26b17228d5e45c315773f0e6dc4a861a18
pgambino-espear <pgambino@espear.com>
2020-05-20 19:43:13 -0400

    Solved all Day 1 problem's except for cookies.

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,22 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
+from itertools import permutations 
 
-    pass
+perm = permutations([1, 2, 3]) 
+  
+# Print the obtained permutations 
+print(perm)
 
-if __name__ == "__main__":
-    # Use the main function here to test out your implementation
-    num_cookies = 5
+for i in list(perm): 
+    print(i)
+# def eating_cookies(n):
+#     # Your code here
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+#     pass
+
+# if __name__ == "__main__":
+#     # Use the main function here to test out your implementation
+#     num_cookies = 5
+
+#     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
